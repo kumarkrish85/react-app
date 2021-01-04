735b9e276dd03a09e6f3bb27b8a22668fa54f468
Nunmuthu <nunmuthu@gmail.com>
2021-01-04 12:12:29 +0530

    Updated redux with react-redux connect

--- src/Components/employee.jsx
+++ src/Components/employee.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 import axios from "../api";
 import "../css/employee.css";
-import store,{VIEW} from "../store";
+import store, { VIEW } from "../store";
 
-export class Employee extends React.Component {
+class Employee extends React.Component {
   employee = [];
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     axios.get("employeeArray").then((data) => {
       console.log(data);
       this.employee = data.data;
@@ -14,7 +15,7 @@ export class Employee extends React.Component {
     });
   }
 
-  eachCard(obj) {
+  eachCard = (obj) => {
     return (
       <div className="card">
         <img
@@ -26,20 +27,21 @@ export class Employee extends React.Component {
             <b>Name: {obj.Name}</b>
           </h4>
           <p>
-            <b>Employee ID:</b> {obj.EmployeeID}
+            <b>Employee ID:</b> {obj.id}
           </p>
           <input
             type="button"
             value="View"
-            onClick={() => this.viewemployee()}
+            onClick={() => this.viewemployee(obj.id)}
           />
         </div>
       </div>
     );
-  }
+  };
 
-  viewemployee = () => {
-    store.dispath({ type: VIEW, value: 2 });
+  viewemployee = (id) => {
+    // store.dispath({ type: VIEW, value: 2 });
+    this.props.viewEmployee(id);
     this.props.history.push("/empDetails");
   };
 
@@ -50,3 +52,13 @@ export class Employee extends React.Component {
     return <div>{displayData}</div>;
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    viewEmployee: (id) => {
+      dispatch({ type: VIEW, value: id });
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Employee);
